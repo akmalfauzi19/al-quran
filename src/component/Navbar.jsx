7345b6bb8c178b9d4dc51226fbae24f63f377ce0
akmalfauzi19 <akmal.fauzi19@gmail.com>
2024-04-03 23:49:58 +0700

    disable search in nav

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -9,13 +9,13 @@ export default function Navbar() {
         setIsOpenNav(!isOpenNav);
     }
 
-    const handleModalSearch = () => {
-        setIsOpenSearch(!isOpenSearch)
-    }
+    // const handleModalSearch = () => {
+    //     setIsOpenSearch(!isOpenSearch)
+    // }
 
-    const handleBlurSearch = () => {
-        setIsOpenSearch(!isOpenSearch)
-    }
+    // const handleBlurSearch = () => {
+    //     setIsOpenSearch(!isOpenSearch)
+    // }
     // https://equran.id/api/v3/search?q=as
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900 sticky top-0">
@@ -31,7 +31,7 @@ export default function Navbar() {
                         </svg>
                         <span className="sr-only">Search</span>
                     </button>
-                    <div className="relative hidden md:block">
+                    {/* <div className="relative hidden md:block">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
@@ -39,7 +39,7 @@ export default function Navbar() {
                             <span className="sr-only">Search icon</span>
                         </div>
                         <input type="text" id="search-navbar" onFocus={handleModalSearch} onBlur={handleBlurSearch} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
-                    </div>
+                    </div> */}
                     <button onClick={handleModal} data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -70,7 +70,7 @@ export default function Navbar() {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to={"/about"}
+                            <NavLink to="https://hafdev-portofolio.vercel.app/" target="_blank"
                                 className={({ isActive }) =>
                                     isActive ? 'block py-2 px-3 text-white md:bg-transparent bg-blue-700 rounded md:text-blue-700 md:p-0 md:dark:text-blue-500'
                                         : 'block py-2 px-3 text-gray-900 rounded md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
@@ -79,14 +79,14 @@ export default function Navbar() {
                             </NavLink>
                         </li>
                     </ul>
-                    <div className="relative mt-3 md:hidden">
+                    {/* <div className="relative mt-3 md:hidden">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
                         <input type="text" id="search-navbar" onFocus={handleModalSearch} onBlur={handleBlurSearch} className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." />
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
